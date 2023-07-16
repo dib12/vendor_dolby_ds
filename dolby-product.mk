d008f37cc28cc30d8a17b944c4cd7c849c32ab38
dib12 <61652361+dib12@users.noreply.github.com>
2023-07-16 08:34:00 +0530

    include sepolicy

--- dolby-product.mk
+++ dolby-product.mk
@@ -5,6 +5,9 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/dolby/ds
 
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    vendor/dolby/ds/sepolicy/vendor
+
 PRODUCT_COPY_FILES += \
     vendor/dolby/ds/proprietary/vendor/etc/dolby/dax-conf.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-conf.xml \
     vendor/dolby/ds/proprietary/system_ext/etc/permissions/com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.dolby.daxservice.xml \
